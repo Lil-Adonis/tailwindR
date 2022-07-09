d125b4de24dfed87f7858f574789825f0c0cd90f
EasyleeBCC <adenugaib24@gmail.com>
2022-07-09 18:28:46 +0100

    #50projects50days project 8

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      primary:'#EE9789',
+    },
+    fontFamily:{
+      sans:['Montserrat', 'sans-serif'],
+
+    }
+  },
+  plugins: [],
+}
